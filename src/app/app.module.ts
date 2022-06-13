af05bf8013b16dc9fa242873d2fa12cebb0d4f47
BaptisteTINTI <taillefumierbaptsite@gmail.com>
2022-06-13 11:24:39 +0200

    correction bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,6 @@ import { CompteresponsableComponent } from './compteresponsable/compteresponsabl
 import { CompteadministrateurComponent } from './compteadministrateur/compteadministrateur.component';
 import { ComptechauffeurComponent } from './comptechauffeur/comptechauffeur.component';
 import { TaxiComponent } from './taxi/taxi.component';
-import { SortparamsDirective } from './sortparams.directive';
-import { SortPipe } from './sort.pipe';
-import { FilterPipe } from './filter.pipe';
 
 
 @NgModule({
@@ -49,9 +46,6 @@ import { FilterPipe } from './filter.pipe';
     ComptechauffeurComponent,
     CompteadministrateurComponent,
     TaxiComponent,
-    SortparamsDirective,
-    SortPipe,
-    FilterPipe,
   ],
   providers: [],
   bootstrap: [AppComponent]
