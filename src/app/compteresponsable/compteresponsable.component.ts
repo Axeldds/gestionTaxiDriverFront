cf54d673af22abdfb8c099f86fead7c9675d8d91
Axeldds <axel.didisse@gmail.com>
2022-06-14 15:47:39 +0200

    liaison du formulaire Responsable a la base de données

--- src/app/compteresponsable/compteresponsable.component.ts
+++ src/app/compteresponsable/compteresponsable.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponsableAgence } from 'app/modules/responsable-agence';
+import { Utilisateur } from 'app/modules/utilisateur';
+import { ResponsableService } from 'app/services/responsable.service';
+import { UtilisateurService } from 'app/services/utilisateur.service';
 
 @Component({
   selector: 'app-compteresponsable',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compteresponsable.component.scss']
 })
 export class CompteresponsableComponent implements OnInit {
+  responsables!: any[];
+  responsable: ResponsableAgence =new ResponsableAgence();
 
-  constructor() { }
+  utilisateurs!: any[];
+  utilisateur: Utilisateur = new Utilisateur();
+  constructor(private responsableService:ResponsableService, private utilisateurService:UtilisateurService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.findAll();
+    this.findAllUser();
   }
 
+  findAll(){
+  this.responsableService.findAll().subscribe(data => {this.responsables = data});
+  }
+
+  findAllUser(){
+    this.utilisateurService.findAll().subscribe(data => {this.utilisateur = data});
+  }
+  deleteResp(id:number){
+    this.responsableService.delete(id).subscribe(() => {this.findAll()});
+  }
+
+  save(){
+    this.responsableService.save(this.responsable).subscribe(() => {
+      this.findAll();
+      this.responsable =new ResponsableAgence();
+    })
+    this.utilisateurService.save(this.utilisateur).subscribe(() => {
+      this.findAll();
+      this.utilisateur = new Utilisateur
+    })
+  }
 }
