7880abb15e678bd8d972e5960d932b891654612b
root <root@localhost.localdomain>
2022-06-15 11:27:10 +0200

    page annonce v1

--- src/app/annonce/annonce.component.ts
+++ src/app/annonce/annonce.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Annonce } from 'app/modules/annonce';
+import { AnnonceService } from 'app/services/annonce.service';
 
 @Component({
   selector: 'app-annonce',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnonceComponent implements OnInit {
 
-  constructor() { }
+  annonces!:any[]
+  annonce : Annonce=new Annonce();
 
-  ngOnInit(): void {
+  constructor(private annonceService:AnnonceService) {}
+
+  ngOnInit(): void {    
+    this.findAllAnnonce();
+  }
+  findAllAnnonce(){
+    this.annonceService.findAll().subscribe(data => {this.annonces = data});
   }
 
-}
+
+  deleteAnnonce(id:number){
+    this.annonceService.delete(id).subscribe(() => {this.findAllAnnonce()});
+  }
+
+  saveAnnonce(){
+    this.annonceService.save(this.annonce).subscribe(() => {
+      this.findAllAnnonce();
+      this.annonce = new Annonce();
+    })
+  }
+  }
+
+
