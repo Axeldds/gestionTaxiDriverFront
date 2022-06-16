89ec86827792f518b6dc0b85a338aa3480c62850
Axeldds <axel.didisse@gmail.com>
2022-06-16 16:25:46 +0200

    fonction cacher/afficher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app.routing';
@@ -33,6 +33,8 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { AgenceComponent } from './agence/agence.component';
 import { LoginComponent } from './login/login.component';
 import { PlanningComponent } from './planning/planning.component';
+import { Observable } from 'rxjs';
+import { AppService } from './app.service';
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
@@ -41,6 +43,16 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   interactionPlugin
 ]);
 
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor{
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+ const xhr=req.clone({
+  headers: req.headers.set('X-Requested-With','XMLHttpRequest')
+ });   
+    return next.handle(xhr);
+  }
+  
+}
 
 @NgModule({
   imports: [
@@ -75,7 +87,10 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     LoginComponent,
     PlanningComponent,
   ],
-  providers: [],
+  providers: [
+    AppService,
+    {provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
